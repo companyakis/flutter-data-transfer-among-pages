d3e0680baec7f12a9d3142f7e4e8a0d06e385398
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-16 20:38:35 +0300

    Create employee_class.dart

---
+++ employee_class.dart
@@ -0,0 +1,15 @@
+class Employee {
+  String name;
+  String department;
+  int id;
+  int birthYear;
+  bool isAged;
+
+  Employee({
+    required this.name,
+    required this.department,
+    required this.id,
+    required this.birthYear,
+    required this.isAged,
+  });
+}
